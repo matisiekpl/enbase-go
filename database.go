475c5d655e7597766b43a73b9aff533885c562cf
Unknown <matisiek11@gmail.com>
2018-11-21 22:09:40 +0100

    :bug: Fixed bug with mongodb on docker

--- database.go
+++ database.go
@@ -9,14 +9,18 @@ var databaseSession *mgo.Session
 var applicationDatabase *mgo.Database
 
 func connectToDatabase() {
-	mongoUrl := os.Getenv("mongo")
+	mongoUrl := os.Getenv("MONGO")
+	mongoName := os.Getenv("MONGO_NAME")
 	if mongoUrl == "" {
 		mongoUrl = "localhost:27017"
+	}
+	if mongoName == "" {
+		mongoName = "__enbase"
 	}
 	session, err := mgo.Dial(mongoUrl)
 	if err != nil {
 		panic(err)
 	}
 	databaseSession = session
-	applicationDatabase = session.DB("__enbase")
+	applicationDatabase = session.DB(mongoName)
 }
