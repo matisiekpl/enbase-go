63bf0bb2dd4c297ae292410c573c4bcaac829147
Mateusz Woźniak <matisiek11@gmail.com>
2019-04-20 12:31:08 +0200

    :art: Removed all code files

--- auth.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type User struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
-	Role     string `json:"role" validate:"required"`
-}
--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
 func main() {
-	connectToDatabase()
-	connectToPubSub()
-	bootstrapRestServer()
-	defer changesChannel.Close()
-	defer pubsub.Close()
-	rest.Logger.Fatal(rest.Start(":1323"))
+	
 }
